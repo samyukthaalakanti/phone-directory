fa5bcde97530a7d453bbef1a68e9354e36e61af7
Samyuktha <samyugupta@gmail.com>
2022-03-03 18:58:41 +0530

    Created skeleton of home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-
+//import './App.css'
 function App() {
   return (
     <div>
-    Phone Directory
-    </div>
+     <div className="container">
+       {/* <h1>Hello World</h1> */}
+       {/* <input type="text" placeholder="Type Here"/> */}
+     Phone Directory 
+     </div>
+     <button>Add</button>
+     <span>Name</span><br/>
+     <span>Phone</span>
+     {/* <h1>Hello World</h1><br/>
+     <h1>Hello World2</h1>
+     <label htmlFor="name">Name :</label>
+     <input type="text" name="name"/><br/>
+     <label htmlFor="age">Age :</label>
+     <input type="text"/> */}
+     </div>
   );
 }
 
