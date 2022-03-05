72449207dc3ca3f78b91fe82e3e29de903f700f9
Samyuktha <samyugupta@gmail.com>
2022-03-05 12:36:08 +0530

    Displayed Phone Directory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-//import './App.css'
-function App() {
-  return (
-    <div>
-     <div className="container">
-       {/* <h1>Hello World</h1> */}
-       {/* <input type="text" placeholder="Type Here"/> */}
-     Phone Directory 
-     </div>
-     <button>Add</button>
-     <span>Name</span><br/>
-     <span>Phone</span>
-     {/* <h1>Hello World</h1><br/>
-     <h1>Hello World2</h1>
-     <label htmlFor="name">Name :</label>
-     <input type="text" name="name"/><br/>
-     <label htmlFor="age">Age :</label>
-     <input type="text"/> */}
-     </div>
-  );
-}
 
-export default App;
+   function App() {
+     return(
+       <div>
+         Phone Directory
+       </div>
+     );
+   }
+
+   export default App;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
 
-    <App />
-  ,
-  document.getElementById('root')
+    <App/> , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
